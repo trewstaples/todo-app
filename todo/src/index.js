bdc17ec338cd785dfce7c653baec499aa452556d
LAlex <trewstaples@gmail.com>
2022-04-13 14:46:32 +0300

    Разделит компоненты

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const el = (
-  <div>
-    <h1>Todo App</h1>
-    <input placeholder="search" />
+const AppHeader = () => {
+  return <h1>Todo App</h1>;
+};
+
+const SearchPanel = () => {
+  return <input placeholder="search" />;
+};
+
+const TodoList = () => {
+  return (
     <ul>
       <li>Learn React App</li>
       <li>Drink Coffee</li>
     </ul>
+  );
+};
+
+const el = (
+  <div>
+    <AppHeader />
+    <SearchPanel />
+    <TodoList />
   </div>
 );
 
