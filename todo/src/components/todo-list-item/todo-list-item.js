19ad31db00b3ad279f690b3e3338643c5642397b
LAlex <trewstaples@gmail.com>
2022-04-15 22:37:39 +0300

    Добавит localState у todo-list-item

--- todo/src/components/todo-list-item/todo-list-item.js
+++ todo/src/components/todo-list-item/todo-list-item.js
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './todo-list-item.css';
 
-const TodoListItem = ({ label, important = false }) => {
-  const style = {
-    color: important ? 'steelblue' : 'black',
-    fontWeight: important ? 'bold' : 'normal',
+const TodoListItem = ({ label }) => {
+  const [done, setDone] = useState(false);
+  const [important, setImportant] = useState(false);
+
+  const onLabelClick = () => {
+    setDone(!done);
+  };
+
+  const onMarkImportant = () => {
+    setImportant(!important);
   };
 
   return (
-    <span className="todo-list-item">
-      <span className="todo-list-item-label" style={style}>
+    <span className={`todo-list-item ${done && 'done'} ${important && 'important'}`}>
+      <span className="todo-list-item-label" onClick={onLabelClick}>
         {label}
       </span>
 
-      <button type="button" className="btn btn-outline-success btn-sm float-right">
+      <button type="button" className="btn btn-outline-success btn-sm float-right" onClick={onMarkImportant}>
         <i className="fa fa-exclamation" />
       </button>
 
