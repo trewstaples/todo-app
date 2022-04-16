b81568f3fa32d54260ae03533616defbb1bb30b3
LAlex <trewstaples@gmail.com>
2022-04-16 19:31:05 +0300

    Настроит добавление новых элементов

--- todo/src/components/item-add-form/item-add-form.js
+++ todo/src/components/item-add-form/item-add-form.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import './item-add-form.css';
 
 const ItemAddForm = ({ onItemAdded }) => {
+  const [formValue, setFormValue] = useState();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    onItemAdded(formValue);
+  };
+
   return (
-    <div className="item-add-form">
-      <button className="btn btn-outline-secondary" onClick={() => onItemAdded('Hello World')}>
-        Add Item
-      </button>
-    </div>
+    <form className="bottom-panel d-flex" onSubmit={onSubmit}>
+      <input
+        type="text"
+        className="form-control new-todo-label"
+        onChange={(e) => setFormValue(e.target.value)}
+        placeholder="What needs to be done?"
+      />
+
+      <button className="btn btn-outline-secondary">Add Item</button>
+    </form>
   );
 };
 
